26ce9902dc9b2a9b5397fcae89fba0f6ac4ac856
Salif SAWADOGO <96554481+sawadogosalif@users.noreply.github.com>
2024-11-24 03:02:11 +0100

    :construction_worker: docs

--- src/jwsoup/package_metadata.py
+++ src/jwsoup/package_metadata.py
@@ -4,5 +4,5 @@ __doc__ = "This package is design to scrape Bible data on JW.org for NLP/IAgen t
 __name__ = "jwsoup"
 __author__ = "Salif SAWADOGO"
 __email__ = "salif.sawadogo.pro@gmail.com"
-__version__ = "0.0.3d"
+__version__ = "0.0.4"
 __url__ = "https://github.com/sawadogosalif/jwsoup.git"
